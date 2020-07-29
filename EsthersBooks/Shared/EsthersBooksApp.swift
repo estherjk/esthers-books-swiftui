811f5c3e967b8671475bbe0b026383a86eb301f9
Esther Jun Kim <hello@esthermakes.tech>
2020-07-29 16:47:25 -0700

    Refactor project as a multiplatform app
    Using Xcode 12 beta. Builds for iPhone, iPad, & desktop!

---
+++ EsthersBooks/Shared/EsthersBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  EsthersBooksApp.swift
+//  Shared
+//
+//  Created by Esther Kim on 7/29/20.
+//
+
+import SwiftUI
+
+@main
+struct EsthersBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Home()
+        }
+    }
+}
