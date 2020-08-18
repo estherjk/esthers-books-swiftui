f7a506e73d79ef7198cf3dc71d2ff890351264d6
Esther Jun Kim <hello@esthermakes.tech>
2020-08-17 17:39:22 -0700

    Cache images using NSCache

---
+++ EsthersBooks/Shared/Models/ImageCache.swift
@@ -0,0 +1,30 @@
+//
+//  ImageCache.swift
+//  EsthersBooks
+//
+//  Created by Esther Kim on 8/17/20.
+//
+
+import Foundation
+import UIKit
+
+// See https://medium.com/macoclock/remote-image-url-with-caching-in-swiftui-611f17cbd985
+class ImageCache {
+    var cache = NSCache<NSString, UIImage>()
+    
+    func get(forKey: String) -> UIImage? {
+        return cache.object(forKey: NSString(string: forKey))
+    }
+    
+    func set(forKey: String, uiImage: UIImage) {
+        cache.setObject(uiImage, forKey: NSString(string: forKey))
+    }
+}
+
+extension ImageCache {
+    private static var imageCache = ImageCache()
+    
+    static func getImageCache() -> ImageCache {
+        return imageCache
+    }
+}
