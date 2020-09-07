781760174e22435dbe71fcd8ad8c6fd29886b919
Esther Jun Kim <3721990+estherjk@users.noreply.github.com>
2020-09-07 10:58:40 -0700

    Animate icon on splash view

--- EsthersBooks/Shared/Splash/SplashView.swift
+++ EsthersBooks/Shared/Splash/SplashView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct SplashView: View {
+    @State private var opacity = 1.0
     
     var body: some View {
         // TODO: Make a fancy animation
@@ -16,6 +17,9 @@ struct SplashView: View {
             .aspectRatio(contentMode: .fit)
             .foregroundColor(Color.accentColor)
             .frame(width: 100, height: 100, alignment: .center)
+            .opacity(opacity)
+            .animation(Animation.easeIn(duration: 1).repeatForever(autoreverses: true))
+            .onAppear { self.opacity = 0.2 }
     }
 }
 
