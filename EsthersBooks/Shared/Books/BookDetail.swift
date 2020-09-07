7612e7797de40b0d916d67daf4e2a6438743256a
Esther Jun Kim <hello@esthermakes.tech>
2020-09-06 19:20:02 -0700

    Update BookDetail
    Includes Information section (replaces old date range) and Genre section.

--- EsthersBooks/Shared/Books/BookDetail.swift
+++ EsthersBooks/Shared/Books/BookDetail.swift
@@ -9,34 +9,69 @@
 import SwiftUI
 
 struct BookDetail: View {
+    @EnvironmentObject var genresAPI: GenresAPI
+    
     // Format date inside text views
     // See https://www.hackingwithswift.com/quick-start/swiftui/how-to-format-text-inside-text-views
     static let dateFormatter: DateFormatter =  {
         let formatter = DateFormatter()
-        formatter.dateStyle = .long
+        formatter.dateStyle = .medium
         return formatter
     }()
     
     let book: Book
     
     var body: some View {
-        // TODO: Replace hard-coded data
-        VStack(spacing: 5) {
-            Spacer()
-            
-            BookCover(imageURL: book.cover_attachment.url)
-                .padding(.bottom)
-            
-            Text(book.title)
-                .font(.headline)
-                .multilineTextAlignment(.center)
-                .padding(.leading)
-                .padding(.trailing)
-            
-            Text("\(book.date_started, formatter: Self.dateFormatter) to \(book.date_finished, formatter: Self.dateFormatter)")
-                .font(.subheadline)
-            
-            Spacer()
+        ScrollView(/*@START_MENU_TOKEN@*/.vertical/*@END_MENU_TOKEN@*/) {
+            VStack(spacing: 5) {
+                BookCover(imageURL: book.cover_attachment.url)
+                    .padding(.bottom)
+                
+                Text(book.title)
+                    .font(.title3)
+                    .multilineTextAlignment(.center)
+                    .padding(.leading)
+                    .padding(.trailing)
+                
+                Divider()
+                    .frame(width: 300)
+                    .padding()
+                
+                Text("Information")
+                    .font(.headline)
+                
+                HStack {
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Text("Date Started:")
+                                .foregroundColor(.secondary)
+                            
+                            Text("\(book.date_started, formatter: Self.dateFormatter)")
+                        }
+                        
+                        HStack {
+                            Text("Date Finished:")
+                                .foregroundColor(.secondary)
+                            
+                            Text("\(book.date_finished, formatter: Self.dateFormatter)")
+                        }
+                    }
+                }
+                
+                Divider()
+                    .frame(width: 300)
+                    .padding()
+                
+                Text("Genres")
+                    .font(.headline)
+                
+                ForEach(0 ..< book.genres.count) { index in
+                    let genre = genresAPI.genres.filter({ $0.trello_id == book.genres[index] })
+                    Text(genre[0].name)
+                }
+                
+                Spacer()
+            }
         }
     }
 }
@@ -54,5 +89,6 @@ struct BookCover: View {
 struct BookDetail_Previews: PreviewProvider {
     static var previews: some View {
         BookDetail(book: sampleBooksData[4])
+            .environmentObject(GenresAPI())
     }
 }
